be25b35adaa9638879d04646deddadb788f345a4
j178 <10510431+j178@users.noreply.github.com>
2023-01-02 22:04:34 +0800

    Cleanup nbsp

--- leetcode/question.go
+++ leetcode/question.go
@@ -264,8 +264,8 @@ func (q *QuestionData) GetContent() string {
 
 func (q *QuestionData) GetFormattedContent() string {
 	// TODO 处理上标、下标
-	// TODO 处理 NBSP
 	content := q.GetContent()
+	content = strings.ReplaceAll(content, "&nbsp;", "")
 	converter := md.NewConverter("", true, nil)
 	converter.Use(plugin.GitHubFlavored())
 	content, err := converter.ConvertString(content)
