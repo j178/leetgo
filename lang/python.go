306828c5e4659c10691f6e8f63fe2b87cc371815
Frost Ming <me@frostming.com>
2023-02-16 18:23:34 +0800

    support system design testing

--- lang/python.go
+++ lang/python.go
@@ -13,18 +13,71 @@ import (
 )
 
 const (
-	requirements   = "pytest>=7\n"
-	pytestTemplate = `import pytest
-
-from solution import Solution
-
-TEST_CASES = %s
+	requirements = "pytest>=7\n"
+	confTest     = `import json
 
 
-@pytest.mark.parametrize("input_args,expected", TEST_CASES)
+def _parse_test_cases(test_cases_str):
+	inputs, outputs = [], []
+	current = inputs
+	for line in test_cases_str.splitlines():
+		line = line.strip()
+		if not line:
+			if inputs:
+				yield inputs, outputs
+				inputs, outputs = [], []
+		elif line.startswith("input:"):
+			current = inputs
+		elif line.startswith("output:"):
+			current = outputs
+		else:
+			current.append(json.loads(line))
+	if inputs:
+		yield inputs, outputs
+
+
+def pytest_generate_tests(metafunc):
+	if "input_args" not in metafunc.fixturenames:
+		return
+	arg_names = ["input_args", "expected"]
+	if "methods" in metafunc.fixturenames:
+		arg_names.append("methods")
+	matrix = []
+	for inputs, outputs in _parse_test_cases(metafunc.module.TEST_CASES):
+		if "methods" in metafunc.fixturenames:
+			methods, inputs = inputs
+			matrix.append([inputs, outputs[0], methods])
+		else:
+			# Assume there is only one output value
+			matrix.append([inputs, outputs[0]])
+	metafunc.parametrize(arg_names, matrix)
+`
+	pytestTemplate = `from solution import Solution
+
+TEST_CASES = """\
+%s
+"""
+
+
 def test_solution(input_args, expected):
 	result = Solution().%s(*input_args)
 	assert result == expected
+`
+	pytestSystemTemplate = `from solution import Solution
+
+TEST_CASES = """\
+%s
+"""
+
+
+def test_solution(input_args, expected, methods):
+    assert methods[0] == "Solution"
+    solution = Solution(*input_args[0])
+    results = [None]
+    for method, args in zip(methods[1:], input_args[1:]):
+        result = getattr(solution, method)(*args)
+        results.append(result)
+    assert results == expected
 `
 )
 
@@ -42,6 +95,10 @@ func (p python) Initialize(outDir string) error {
 	}
 
 	_, err = tryWrite(path.Join(outDir, "requirements.txt"), requirements)
+	if err != nil {
+		return err
+	}
+	_, err = tryWrite(path.Join(outDir, "conftest.py"), confTest)
 	if err != nil {
 		return err
 	}
@@ -103,7 +160,11 @@ func (g python) Generate(q *leetcode.QuestionData) (*GenerateResult, error) {
 }
 
 func (p python) generateTest(q *leetcode.QuestionData, testcases string) string {
-	return fmt.Sprintf(pytestTemplate, testcases, q.MetaData.Name)
+	if q.MetaData.SystemDesign {
+		return fmt.Sprintf(pytestSystemTemplate, testcases)
+	} else {
+		return fmt.Sprintf(pytestTemplate, testcases, q.MetaData.Name)
+	}
 }
 
 func (p python) generateTestCases(q *leetcode.QuestionData) string {
@@ -117,16 +178,15 @@ func (p python) generateTestCases(q *leetcode.QuestionData) string {
 	}
 
 	// Assume all questions output are single.
-	caseAndOutputs := []string{"["}
+	var caseAndOutputs []string
 	for i := 0; i < len(cases) && i/argsNum < len(outputs); i += argsNum {
-		input := strings.Join(cases[i:i+argsNum], ", ")
+		input := strings.Join(cases[i:i+argsNum], "\n")
 		caseAndOutputs = append(
 			caseAndOutputs,
-			fmt.Sprintf("    ([%s], %s),", input, outputs[i/argsNum]),
+			fmt.Sprintf("%s\n%s\n%s\n%s", testCaseInputMark, input, testCaseOutputMark, outputs[i/argsNum]),
 		)
 	}
-	caseAndOutputs = append(caseAndOutputs, "]")
-	return strings.Join(caseAndOutputs, "\n")
+	return strings.Join(caseAndOutputs, "\n\n")
 }
 
 func (p python) GeneratePaths(q *leetcode.QuestionData) (*GenerateResult, error) {
@@ -157,11 +217,16 @@ func (p python) GeneratePaths(q *leetcode.QuestionData) (*GenerateResult, error)
 }
 
 func (p python) RunLocalTest(q *leetcode.QuestionData, outDir string) (bool, error) {
-	cmd := exec.Command(path.Join(outDir, ".venv", config.VenvPython), "-m", "pytest")
+	filenameTmpl := getFilenameTemplate(q, p)
+	baseFilename, err := q.GetFormattedFilename(p.slug, filenameTmpl)
+	if err != nil {
+		return false, err
+	}
+	cmd := exec.Command(path.Join(outDir, ".venv", config.VenvPython), "-m", "pytest", baseFilename)
 	cmd.Dir = outDir
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 
 	return err == nil, nil
 }
