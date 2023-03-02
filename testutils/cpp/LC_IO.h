bb909ca6720dae93e07ea923231445e0866326ff
Ray Wang <raywang777@foxmail.com>
2023-03-02 09:28:34 +0800

    fix indent

--- testutils/cpp/LC_IO.h
+++ testutils/cpp/LC_IO.h
@@ -59,8 +59,8 @@ struct TreeNode {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 /**
