bba16c32aa089ded4e9df702addd8f168572014e
Ray Wang <raywang777@foxmail.com>
2023-03-02 10:12:50 +0800

    change indent from tab to spaces

--- testutils/cpp/LC_IO.h
+++ testutils/cpp/LC_IO.h
@@ -8,33 +8,33 @@
  * Definition for a singly-linked list.
  */
 struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode() : val(0), next(nullptr) {}
-	ListNode(int x) : val(x), next(nullptr) {}
-	ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 /**
  * Function for deserializing a singly-linked list.
  */
 std::istream &operator>>(std::istream &is, ListNode *&node) {
-	node = nullptr;
-	ListNode *now = nullptr;
+    node = nullptr;
+    ListNode *now = nullptr;
 L0: is.ignore();
 L1: switch (is.peek()) {
-	case ' ':
-	case ',': is.ignore(); goto L1;
-	case ']': is.ignore(); goto L2;
-	default : int x; is >> x;
-	          now = (now ? now->next : node) = new ListNode(x);
-	          goto L1;
-	}
+    case ' ':
+    case ',': is.ignore(); goto L1;
+    case ']': is.ignore(); goto L2;
+    default : int x; is >> x;
+              now = (now ? now->next : node) = new ListNode(x);
+              goto L1;
+    }
 L2: switch (is.peek()) {
-	case '\r': is.ignore(); goto L2;
-	case '\n': is.ignore(); goto L3;
-	case EOF : goto L3;
-	}
+    case '\r': is.ignore(); goto L2;
+    case '\n': is.ignore(); goto L3;
+    case EOF : goto L3;
+    }
 L3: return is;
 }
 
@@ -42,89 +42,89 @@ L3: return is;
  * Function for serializing a singly-linked list.
  */
 std::ostream &operator<<(std::ostream &os, ListNode *node) {
-	os << '[';
-	while (node != nullptr) {
-		os << node->val << ',';
-		node = node->next;
-	}
-	os.seekp(-1, std::ios_base::end);
-	os << ']';
-	return os;
+    os << '[';
+    while (node != nullptr) {
+        os << node->val << ',';
+        node = node->next;
+    }
+    os.seekp(-1, std::ios_base::end);
+    os << ']';
+    return os;
 }
 
 /**
  * Definition for a binary tree node.
  */
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 /**
  * Function for deserializing a binary tree.
  */
 std::istream &operator>>(std::istream &is, TreeNode *&node) {
-	std::deque<TreeNode *> dq;
+    std::deque<TreeNode *> dq;
 L0: is.ignore();
 L1: switch (is.peek()) {
-	case ' ':
-	case ',': is.ignore(); goto L1;
-	case 'n': is.ignore(5); dq.emplace_back(nullptr);
-	          goto L1;
-	case ']': is.ignore(); goto L2;
-	default : int x; is >> x;
-	          dq.emplace_back(new TreeNode(x));
-	          goto L1;
-	}
+    case ' ':
+    case ',': is.ignore(); goto L1;
+    case 'n': is.ignore(5); dq.emplace_back(nullptr);
+              goto L1;
+    case ']': is.ignore(); goto L2;
+    default : int x; is >> x;
+              dq.emplace_back(new TreeNode(x));
+              goto L1;
+    }
 L2: switch (is.peek()) {
-	case '\r': is.ignore(); goto L2;
-	case '\n': is.ignore(); goto L3;
-	case EOF : goto L3;
-	}
+    case '\r': is.ignore(); goto L2;
+    case '\n': is.ignore(); goto L3;
+    case EOF : goto L3;
+    }
 L3: int n = dq.size();
-	for (int i = 0, j = 1; i < n; ++i) {
-		auto root = dq[i];
-		if (root == nullptr) { continue; }
-		root->left = j < n ? dq[j] : nullptr;
-		root->right = j + 1 < n ? dq[j + 1] : nullptr;
-		j += 2;
-	}
-	node = n ? dq[0] : nullptr;
-	return is;
+    for (int i = 0, j = 1; i < n; ++i) {
+        auto root = dq[i];
+        if (root == nullptr) { continue; }
+        root->left = j < n ? dq[j] : nullptr;
+        root->right = j + 1 < n ? dq[j + 1] : nullptr;
+        j += 2;
+    }
+    node = n ? dq[0] : nullptr;
+    return is;
 }
 
 /**
  * Function for serializing a binary tree.
  */
 std::ostream &operator<<(std::ostream &os, TreeNode *node) {
-	std::queue<TreeNode *> q;
-	int cnt_not_null_nodes = 0;
-	auto push = [&](TreeNode *node) {
-		q.emplace(node);
-		if (node != nullptr) { ++cnt_not_null_nodes; }
-	};
-	auto pop = [&]() {
-		auto front = q.front(); q.pop();
-		if (front != nullptr) {
-			--cnt_not_null_nodes;
-			push(front->left);
-			push(front->right);
-			os << front->val << ',';
-		} else {
-			os << "null,";
-		}
-	};
-	os << '[';
-	if (node != nullptr) {
-		push(node);
-		while (cnt_not_null_nodes > 0) { pop();	}
-		os.seekp(-1, std::ios_base::end);
-	}
-	os << ']';
-	return os;
+    std::queue<TreeNode *> q;
+    int cnt_not_null_nodes = 0;
+    auto push = [&](TreeNode *node) {
+        q.emplace(node);
+        if (node != nullptr) { ++cnt_not_null_nodes; }
+    };
+    auto pop = [&]() {
+        auto front = q.front(); q.pop();
+        if (front != nullptr) {
+            --cnt_not_null_nodes;
+            push(front->left);
+            push(front->right);
+            os << front->val << ',';
+        } else {
+            os << "null,";
+        }
+    };
+    os << '[';
+    if (node != nullptr) {
+        push(node);
+        while (cnt_not_null_nodes > 0) { pop();    }
+        os.seekp(-1, std::ios_base::end);
+    }
+    os << ']';
+    return os;
 }
 
 /**
@@ -134,21 +134,21 @@ template <typename T>
 std::istream &operator>>(std::istream &is, std::vector<T> &v) {
 L0: is.ignore();
 L1: switch (is.peek()) {
-	case ' ':
-	case ',': is.ignore(); goto L1;
-	case ']': is.ignore(); goto L2;
-	default : v.emplace_back();
-	          if constexpr (std::is_same_v<T, std::string>) {
-	              is >> quoted(v.back());
-	          } else {
-	              is >> v.back();
-	          }
-	          goto L1;
-	}
+    case ' ':
+    case ',': is.ignore(); goto L1;
+    case ']': is.ignore(); goto L2;
+    default : v.emplace_back();
+              if constexpr (std::is_same_v<T, std::string>) {
+                  is >> quoted(v.back());
+              } else {
+                  is >> v.back();
+              }
+              goto L1;
+    }
 L2: switch (is.peek()) {
-	case '\r': is.ignore(); goto L2;
-	case '\n': is.ignore(); goto L3;
-	case EOF : goto L3;
+    case '\r': is.ignore(); goto L2;
+    case '\n': is.ignore(); goto L3;
+    case EOF : goto L3;
 }
 L3: return is;
 }
@@ -158,19 +158,19 @@ L3: return is;
  */
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-	os << '[';
-	if constexpr (std::is_same_v<T, std::string>) {
-		for (auto &&x : v) { os << quoted(x) << ','; }
-	} else if constexpr (std::is_same_v<T, double>) {
-		for (auto &&x : v) {
-			char buf[320]; sprintf(buf, "%.5f,", x); os << buf;
-		}
-	} else {
-		for (auto &&x : v) { os << x << ','; }
-	}
-	os.seekp(-1, std::ios_base::end);
-	os << ']';
-	return os;
+    os << '[';
+    if constexpr (std::is_same_v<T, std::string>) {
+        for (auto &&x : v) { os << quoted(x) << ','; }
+    } else if constexpr (std::is_same_v<T, double>) {
+        for (auto &&x : v) {
+            char buf[320]; sprintf(buf, "%.5f,", x); os << buf;
+        }
+    } else {
+        for (auto &&x : v) { os << x << ','; }
+    }
+    os.seekp(-1, std::ios_base::end);
+    os << ']';
+    return os;
 }
 
 #endif
