cf59e80a878b092f80a4f218aa6bc6899160e4ed
w43322 <raywang777@foxmail.com>
2023-04-16 12:57:48 +0800

    fix: modify stdc++.h for compatibility with clang (#148)
    * modify stdc++.h for compatibility with clang
    
    * add stdcxx check

--- testutils/cpp/stdc++.h
+++ testutils/cpp/stdc++.h
@@ -54,11 +54,11 @@
 #include <ccomplex>
 #include <cfenv>
 #include <cinttypes>
-#include <cstdalign>
+// #include <cstdalign>
 #include <cstdbool>
 #include <cstdint>
 #include <ctgmath>
-#include <cuchar>
+// #include <cuchar>
 #endif
 
 // C++
@@ -128,7 +128,7 @@
 // #include <execution>
 #include <filesystem>
 #include <optional>
-#include <memory_resource>
+// #include <memory_resource>
 #include <string_view>
 #include <variant>
 #endif
