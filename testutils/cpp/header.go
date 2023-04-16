aaa303e5aedfa1c465b12f269924f778373aa361
w43322 <raywang777@foxmail.com>
2023-04-16 09:40:21 +0800

    modify stdc++.h for compatibility with clang

--- testutils/cpp/header.go
+++ testutils/cpp/header.go
@@ -10,6 +10,7 @@ import (
 const HeaderName = "LC_IO.h"
 
 // StdCxxContent is stdc++.h from gcc 12.2.0
+// "cstdalign", "cuchar", "memory_resource" commented out for compatibility with clang
 // https://github.com/gcc-mirror/gcc/raw/releases/gcc-12.2.0/libstdc%2B%2B-v3/include/precompiled/stdc%2B%2B.h
 //
 //go:embed stdc++.h
