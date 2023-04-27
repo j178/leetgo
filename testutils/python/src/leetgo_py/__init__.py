96e8526890c22853d55e275de0f2daed8debc239
shiyang07ca <iceyasha@gmail.com>
2023-04-27 10:40:44 +0800

    fix: local Python test utils overwriting built-in Python methods (#158)

--- testutils/python/src/leetgo_py/__init__.py
+++ testutils/python/src/leetgo_py/__init__.py
@@ -2,3 +2,14 @@ from .list import ListNode
 from .tree import TreeNode
 from .parse import split_array, deserialize, serialize
 from .utils import join_array, read_line
+
+
+__all__ = [
+    "ListNode",
+    "TreeNode",
+    "split_array",
+    "deserialize",
+    "serialize",
+    "join_array",
+    "read_line",
+]
