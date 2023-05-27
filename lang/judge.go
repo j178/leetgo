89404e2ace0b37e7d0cf1f05d39694b4ced33b56
j178 <10510431+j178@users.noreply.github.com>
2023-05-27 23:09:58 +0800

    feat: enhanced judger

--- lang/judge.go
+++ lang/judge.go
@@ -1,6 +1,7 @@
 package lang
 
 import (
+	"fmt"
 	"math"
 	"strconv"
 	"strings"
@@ -11,58 +12,79 @@ import (
 	goutils "github.com/j178/leetgo/testutils/go"
 )
 
+type JudgeResult interface {
+	IsAccepted() bool
+	GetInfo() string
+}
+
+type simpleResult struct {
+	accepted bool
+	info     string
+}
+
+func failed(info string) JudgeResult {
+	return simpleResult{false, info}
+}
+
+func accepted() JudgeResult {
+	return simpleResult{true, ""}
+}
+
+func (r simpleResult) IsAccepted() bool {
+	return r.accepted
+}
+
+func (r simpleResult) GetInfo() string {
+	return r.info
+}
+
 type Judger interface {
-	Judge(actual, expected string) bool
+	Judge(input []string, output, actualOutput string) JudgeResult
 }
 
-type judgeFunc func(actual, expected string) bool
+type stringJudger struct{}
 
-func (f judgeFunc) Judge(actual, expected string) bool {
-	return f(actual, expected)
-}
-
-type systemDesignJudger struct {
-	q *leetcode.QuestionData
-}
-
-func (systemDesignJudger) Judge(actual, expected string) bool {
-	return stringCompare(actual, expected)
-}
-
-func stringCompare(actual, expected string) bool {
-	return actual == expected
+func (stringJudger) Judge(input []string, output, actualOutput string) JudgeResult {
+	if output != actualOutput {
+		return failed(fmt.Sprintf("expected %q, got %q", output, actualOutput))
+	}
+	return accepted()
 }
 
 type sliceJudger struct {
 	ignoreOrder bool
+	subJudger   Judger
 }
 
-func newSliceJudger(q *leetcode.QuestionData) *sliceJudger {
-	ignoreOrder := shouldIgnoreOrder(q)
-	return &sliceJudger{ignoreOrder}
+func newSliceJudger(ignoreOrder bool, subJudger Judger) *sliceJudger {
+	return &sliceJudger{ignoreOrder, subJudger}
 }
 
-func (j *sliceJudger) Judge(actual, expected string) bool {
-	if actual == expected {
-		return true
+func (j *sliceJudger) Judge(input []string, output, actualOutput string) JudgeResult {
+	if output == actualOutput {
+		return accepted()
 	}
 
-	a, _ := goutils.SplitArray(actual)
-	b, _ := goutils.SplitArray(expected)
+	a, _ := goutils.SplitArray(output)
+	b, _ := goutils.SplitArray(actualOutput)
 	if len(a) != len(b) {
-		return false
+		return failed(fmt.Sprintf("expected %d elements, got %d", len(a), len(b)))
 	}
 
 	if j.ignoreOrder {
-		return j.compareIgnoringOrder(a, b)
+		if !j.compareIgnoringOrder(a, b) {
+			return failed(fmt.Sprintf("expected %q, got %q", output, actualOutput))
+		}
+		return accepted()
 	}
 
 	for i := range a {
-		if a[i] != b[i] {
-			return false
+		if r := j.subJudger.Judge(input, a[i], b[i]); !r.IsAccepted() {
+			rr := failed(r.GetInfo() + " at index " + strconv.Itoa(i))
+			return rr
 		}
 	}
-	return true
+	return accepted()
 }
 
 // TODO improve the detection of "any order"
@@ -102,29 +124,71 @@ func (j *sliceJudger) compareIgnoringOrder(actual, expected []string) bool {
 	return true
 }
 
-// floatCompare compares two float numbers. Returns true if the difference is less than 1e-5.
-func floatCompare(actual, expected string) bool {
-	a, _ := strconv.ParseFloat(actual, 64)
-	b, _ := strconv.ParseFloat(expected, 64)
-	return math.Abs(a-b) < 1e-5
+type floatJudger struct{}
+
+func (floatJudger) Judge(input []string, output, actualOutput string) JudgeResult {
+	a, _ := strconv.ParseFloat(output, 64)
+	b, _ := strconv.ParseFloat(actualOutput, 64)
+	if math.Abs(a-b) >= 1e-5 {
+		return failed(fmt.Sprintf("expected %f, got %f", a, b))
+	}
+	return accepted()
+}
+
+type systemDesignJudger struct {
+	judgers map[string]Judger
+}
+
+func newSystemDesignJudger(q *leetcode.QuestionData) *systemDesignJudger {
+	judgers := map[string]Judger{}
+	for _, m := range q.MetaData.Methods {
+		judgers[m.Name] = getJudger(q, m.Return.Type)
+	}
+	return &systemDesignJudger{judgers}
+}
+
+func (s systemDesignJudger) Judge(input []string, output, actualOutput string) JudgeResult {
+	funcs, _ := goutils.SplitArray(input[0])
+	a, _ := goutils.SplitArray(output)
+	b, _ := goutils.SplitArray(actualOutput)
+
+	if len(a) != len(b) || len(a) != len(funcs) {
+		panic("system design input/output not match")
+	}
+
+	for i := 1; i < len(a); i++ {
+		judger := s.judgers[funcs[i]]
+		if judger == nil {
+			panic(fmt.Sprintf("judger for %s not found", funcs[i]))
+		}
+		if r := judger.Judge(input, a[i], b[i]); !r.IsAccepted() {
+			rr := failed(r.GetInfo() + " at index " + strconv.Itoa(i))
+			return rr
+		}
+	}
+	return accepted()
 }
 
 func GetJudger(q *leetcode.QuestionData) Judger {
-	// TODO compare by question rules
-
-	var judger Judger = judgeFunc(stringCompare)
 	if q.MetaData.SystemDesign {
-		judger = &systemDesignJudger{q}
-	} else {
-		resultType := q.MetaData.ResultType()
-		switch resultType {
-		case "double":
-			judger = judgeFunc(floatCompare)
-		default:
-			if strings.HasSuffix(resultType, "[]") {
-				judger = newSliceJudger(q)
-			}
-		}
+		return newSystemDesignJudger(q)
 	}
-	return judger
+	resultType := q.MetaData.ResultType()
+	return getJudger(q, resultType)
+}
+
+func getJudger(q *leetcode.QuestionData, tp string) Judger {
+	switch tp {
+	case "double":
+		return floatJudger{}
+	case "string":
+		return stringJudger{}
+	default:
+		if strings.HasSuffix(tp, "[]") {
+			ignoreOrder := shouldIgnoreOrder(q)
+			subJudger := getJudger(q, tp[:len(tp)-2])
+			return newSliceJudger(ignoreOrder, subJudger)
+		}
+		return stringJudger{}
+	}
 }
